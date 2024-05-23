077b5d5b8f8d38e4f7ccd218d95e8112ee7a3ed5
YASMIN TARGINO DE ALEXANDRE <23111771@CFP127ALUNO.LOCAL>
2024-05-23 11:20:46 -0300

    login

---
+++ js/login.js
@@ -0,0 +1,41 @@
+const button = document.getElementById('login')
+
+
+const validarLogin = async() => {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+
+    const urlLogin = 'https://back-login.vercel.app/usuarios'
+
+    const listUsers = await fetch(urlLogin)
+
+    const objUsers = await listUsers.json()
+
+    if (email == '' || password == '') {
+        alert('Por favor preencha todos os campos!!')
+    } else {
+
+        let validaUser = false
+
+        console.log(objUsers)
+        objUsers.forEach(user => {
+
+            if (user.email == email && user.senha == password) {
+                alert('funciona')
+                validaUser = true
+                window.location.href = '../index/eva.html'
+                console.log(email)
+                console.log(password)
+            }
+        });
+
+        if (!validaUser) {
+            alert('Por favor verifique o email e senha !!')
+        }
+
+    }
+}
+
+button.addEventListener('click', () => {
+    validarLogin()
+})
